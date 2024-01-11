9c551fa090e5a4ae263bd663922628b065c45e1c
anas-hfd <anas1.hfd@gmail.com>
2024-01-11 22:17:50 +0100

    tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple(str, float):
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     ''' returns k & the square of v'''
     return (k, v**2)
