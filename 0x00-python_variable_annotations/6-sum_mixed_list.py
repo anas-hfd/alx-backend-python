1433fead8c70fe368e3e3ee9ceb95fa4bb8f1d8e
anas-hfd <anas1.hfd@gmail.com>
2024-01-11 21:35:47 +0100

    mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''mixed list'''
+
+from typing import List
+
+
+def sum_mixed_list(mxd_list: List[int, float]) -> float:
+    '''returns the sum of list elements'''
+    return sum(mxd_list)
