ef6764112c5d3f1d5454f284d334d91b434cad37
anas-hfd <anas1.hfd@gmail.com>
2024-01-11 23:32:48 +0100

    finally no issues found

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,20 +2,20 @@
 
 '''mypy'''
 
-from typing import Tuple
+from typing import Tuple, List, Union, Any
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    ''' zoom '''
-    zoomed_in: Tuple = [
+def zoom_array(lst: Union[Tuple[Any, ...], List[Any]], factor: int = 2) -> List:
+    '''func'''
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
-
 array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
+
