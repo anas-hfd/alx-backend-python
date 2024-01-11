fc8ed6302f0dc4387ec497b595edac2eb50d7281
anas-hfd <anas1.hfd@gmail.com>
2024-01-11 22:43:57 +0100

    real adv

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" More complex annotations  """
+
+from typing import Mapping, Any, Sequence, Union, TypeVar
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """ getting a value"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
