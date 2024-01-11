aae1a4c78694770cf156fee84b00493b5e39be92
anas-hfd <anas1.hfd@gmail.com>
2024-01-11 23:35:07 +0100

    finally no issues found

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -13,9 +13,9 @@ def zoom_array(lst: Union[Tuple[Any, ...], List[Any]], factor: int = 2) -> List:
     ]
     return zoomed_in
 
+
 array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
-
