b6fdb4738269ce56de268cced44b9080f7e380dd
anas-hfd <anas1.hfd@gmail.com>
2024-01-11 18:26:21 +0100

    add .

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""""basic typed annotation """
+'''basic typed annotation'''
 
 
 def add(a: float, b: float) -> float:
+    '''returns the sum of a & b'''
     return a + b
-
