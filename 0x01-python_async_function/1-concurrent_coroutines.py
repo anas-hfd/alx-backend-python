6106e0575b9f62777b1874c496cacd312cea1d6d
anas-hfd <anas1.hfd@gmail.com>
2024-01-15 22:20:27 +0100

    gather delays

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@
 from typing import List
 import asyncio
 
-wait_random = __import__("0-basic_async_syntax.py").wait_random
+wait_random = __import__('0-basic_async_syntax.py').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
