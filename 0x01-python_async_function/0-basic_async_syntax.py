8f92470b5fb12fc656d00af541f959f09eaeab63
anas-hfd <anas1.hfd@gmail.com>
2024-01-15 21:15:58 +0100

    async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-
+"""asynchronious coroutine"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    # coroutine that waits for a random delay between 0 and
+    """coroutine that waits for a random delay between 0 and"""
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
     return random_delay
