3682ff1d804d12538654c708554723c37c048ca6
anas-hfd <anas1.hfd@gmail.com>
2024-01-15 22:32:31 +0100

    gather delays

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """complex coroutines"""
 
-from typing import List
 import asyncio
+from typing import List
 
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawns wait_random n times with max_delay"""
-    spwn = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*spwn)
-    return sorted(delays)
+    spwn = []
+    for _ in range(n):
+        spwn.append(asyncio.create_task(wait_random(max_delay)))
+    await asyncio.gather(*spwn)
+    return sorted(spwn)
