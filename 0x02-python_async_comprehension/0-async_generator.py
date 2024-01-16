f840186cc4d5c129cebe37b95d60d57e5cdb91ec
anas-hfd <anas1.hfd@gmail.com>
2024-01-16 23:17:54 +0100

    still not a real genertor

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """loop 10 times, wait 1 sec and yield a rand(0-10)"""
     for _ in range(10):
         await asyncio.sleep(1)
