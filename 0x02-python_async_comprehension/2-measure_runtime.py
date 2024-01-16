d6a3e1221e3aa4795e6693cf8d3f7d2f6ca680be
anas-hfd <anas1.hfd@gmail.com>
2024-01-16 23:58:49 +0100

    measure runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-""" measure runtime"""
+"""measure runtime"""
 
+from asyncio import gather
 from time import time
-import asyncio
 
-async_comprehension = __import__("1-async_comprehension").async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """parallel comprehensions"""
+    """ runtime for 4 funcs"""
     start = time()
-    comp_loop = [async_comprehension for i in range(4)]
-    await asyncio.gather(*comp_loop)
+    comp_loop = [async_comprehension() for i in range(4)]
+    await gather(*comp_loop)
     end = time()
-    return end - start
+    return (end - start)
